e95ec50abff52f6f5896810692e3a32779445b5a
André Vinícius dos Santos <32529232+andrevinicius201@users.noreply.github.com>
2023-07-27 20:50:22 -0300

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -1,11 +1,11 @@
 function bubbleSort(array){
   for(let i=0; i<array.length; i++){
-    for(let j=i+1; j<array.length; j++){
-          if(array[i] > array[j]){
-            let aux = array[i]
-            array[i] = array[j]
-            array[j] = aux
-          }
+    for(let j=0; j<array.length; j++){
+      if(array[j] > array[j+1]){
+        let aux = array[j]
+        array[j] = array[j+1]
+        array[j+1] = aux
+      }
     }
 
   }
